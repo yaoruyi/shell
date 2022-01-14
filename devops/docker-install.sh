5c8c8576805ee94393b6d98d239c13b25c415854
yaoruyi <60915472+yaoruyi@users.noreply.github.com>
2022-01-14 11:39:58 +0800

    Update docker-install.sh

--- devops/docker-install.sh
+++ devops/docker-install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-cd ~
 echo -e "\033[32m install pip \033[0m"
 yum -y install python-pip
 echo -e "\033[32m install gen-pac \033[0m"
 pip install -U genpac
+echo -e "\033[32m install docker-compose \033[0m"
+curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+echo -e "\033[32m install docker-up \033[0m"
+docker-compose up --build -d
